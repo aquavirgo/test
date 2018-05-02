344ad641979d866aaabf75e34f1d1be40d01c3e0
Jakub Guzikowski <j.guzikowski@samsung.local>
2018-05-02 08:24:09 +0200

    Nowy plik
    New class
    
    important class

--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 //Klasa odpowiedzialna za iterację
+
 public static Test(){
+public static void main(String[] args){
 int i=1; //poczatek loop
 //chyba tu
 for(i;i<10;i++){
@@ -8,6 +10,20 @@ for(i;i<10;i++){
  }
 //TEST
 }
-void test(){}
+}
+//void test(){}
 
 //CZy to dziala czy nie ???
+<<<<<<< HEAD
+
+public class Tart{
+int i=0;}
+||||||| parent of 76d74a6... important class
+=======
+
+abstract class NewFile{
+Test test = new Test():
+test.main();
+
+}
+>>>>>>> 76d74a6... important class
